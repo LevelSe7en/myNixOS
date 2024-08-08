4846f83055a12ea0da35599ec3dd78c5a9bb97b0
LevelSe7en <seb.doerr@web.de>
2024-08-08 21:38:03 +0200

    Create default.nix

---
+++ hosts/hp-x360/default.nix
@@ -0,0 +1,53 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    inputs.hardware.nixosModules.common-cpu-amd
+    inputs.hardware.nixosModules.common-gpu-amd
+    inputs.hardware.nixosModules.common-pc-ssd
+
+    ./hardware-configuration.nix
+
+    ../common/global
+    ../common/users/sd
+
+#    ../common/optional/peripherals.nix
+#    ../common/optional/greetd.nix
+    ../common/optional/pipewire.nix
+    ../common/optional/quietboot.nix
+
+#    ../common/optional/tlp.nix
+    ../common/optional/wireless.nix
+  ];
+
+  networking = {
+    hostName = "hp-x360";
+  };
+
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
+    binfmt.emulatedSystems = [
+      "aarch64-linux"
+      "i686-linux"
+    ];
+  };
+
+  powerManagement.powertop.enable = true;
+  programs = {
+    light.enable = true;
+    adb.enable = true;
+    dconf.enable = true;
+  };
+
+  # Lid settings
+  services.logind = {
+    lidSwitch = "suspend";
+    lidSwitchExternalPower = "lock";
+  };
+
+  hardware.opengl.enable = true;
+
+  system.stateVersion = "24.05";
+}
